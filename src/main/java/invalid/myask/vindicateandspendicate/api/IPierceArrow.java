3786bef52cd15974a125851f0c3368f39f7aeaae
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2025-08-13 06:42:09 -0600

    update XXL

--- src/main/java/invalid/myask/vindicateandspendicate/api/IPierceArrow.java
+++ src/main/java/invalid/myask/vindicateandspendicate/api/IPierceArrow.java
@@ -1,8 +1,12 @@
 package invalid.myask.vindicateandspendicate.api;
 
+import java.util.UUID;
+
 public interface IPierceArrow {
     void vindicateAndSpendicate$setPierces(int number);
     int vindicateAndSpendicate$getPierces();
 
     int vindicateAndSpendicate$getInitialPierces();
+    void vindicateAndSpendicate$setShotGroupUUID(UUID uuid);
+    UUID vindicateAndSpendicate$getShotGroupUUID();
 }
